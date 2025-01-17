e8fdac736b84d831b0569f591f906fcabd2e7390
adrian <ferrufinoadrian70@gmail.com>
2025-01-17 10:09:09 -0400

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import './App.css'
 function App() {
 
   return (
-    <p>Hola Fred y Andres esta es una web desplegada en Amazon Web Services</p>
+    <div>
+      <h1>Hola Fred y Andres esta es una web desplegada en Amazon Web Services</h1>
+      <h2>Este es el segundo deploy de la app en AWS, añadi una metadescripción</h2>
+      
+    </div>
+    
   )
 }
 
