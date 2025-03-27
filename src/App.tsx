08dc6fb57e6003626be90cf4f458d741d801b482
adrian <ferrufinoadrian70@gmail.com>
2025-03-27 12:27:49 -0400

    Changed App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Aplicacion en React, hecho por Adrian Montaño</h1>
+      
+      
+    </div>
+    
+  )
+}
+
+export default App
