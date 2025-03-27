a9aa6364cdd743b02a3d232d3d7f8e0647dbba28
AMonFer <148228633+AMonFer@users.noreply.github.com>
2025-03-27 12:29:59 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <div>
-      <h1>Hola Fred y Andres esta es una web desplegada en Amazon Web Services</h1>
-      <h2>Este es el segundo deploy de la app en AWS, añadi una metadescripción</h2>
+      <h1>Aplicacion en React, hecho por Adrian Montaño</h1>
+      
       
     </div>
     
